7d4508de1fbd51a11cf57c6e01d9e20b33eae508
Mauro Garro <maurogarro73@gmail.com>
2023-04-22 20:12:05 -0300

    addPoruct funcionandooogit add .git add .

--- desafio2.js
+++ desafio2.js
@@ -1,34 +1,48 @@
+const fs = require('fs');
+
 class ProductManager {
   id = 1;
-
   constructor(path) {
     this.path = path;
-    this.products = [];
   }
 
-  addProduct(productData) {
+  async addProduct(productData) {
+    if (!fs.existsSync(this.path)) {
+      await fs.promises.writeFile(this.path, '[]');
+    }
+
     const { title, description, price, thumbnail, code, stock } = productData;
 
     if (!title || !description || !price || !thumbnail || !code || !stock) {
       return 'complete all fields';
     } else {
-      const productFound = this.products.some((item) => item.code == code);
+      let products = [];
+
+      let productsContent = await fs.promises.readFile(this.path, 'utf-8');
+      products = JSON.parse(productsContent);
+
+      const productFound = products.some((item) => item.code == code);
       if (productFound) {
         return 'The product already exists';
       } else {
+        if (products.length > 0) {
+          this.id = products[products.length - 1].id + 1;
+        }
         const product = { id: this.id, ...productData };
-        this.products.push(product);
-        this.id++;
+        products.push(product);
+        let productString = JSON.stringify(products, null, 2);
+        await fs.promises.writeFile(this.path, productString);
         return 'Added product!';
       }
     }
   }
 
-  getProducts() {
+  async getProducts() {
+    fs.existsSync('');
     return this.products;
   }
 
-  getProductById(id) {
+  async getProductById(id) {
     const productFound = this.products.find((item) => item.id == id);
     if (productFound) {
       console.log('The found product is ');
@@ -49,25 +63,28 @@ const product1 = {
 };
 
 const product2 = {
-  title: 'producto prueba',
-  description: 'Este es un producto prueba',
+  title: 'producto prueba 2',
+  description: 'Este es un producto prueba 2',
   price: 10,
   thumbnail: 'Sin imagen',
   code: 'abc124',
-  stock: 7,
+  stock: 15,
 };
 
 const productManager = new ProductManager('products.json');
-console.log(productManager.addProduct(product1));
-console.log(productManager.addProduct(product2));
-
-console.log(productManager.getProducts());
-
-console.log(productManager.getProductById(2));
-
-/* console.log(product1.addProduct('producto prueba', 'Este es un producto prueba', 10, 'Sin imagen', 'abc123', 1)); */
-/* console.log(product1.addProduct('producto prueba', 'Este es un producto prueba', 10, 'Sin imagen', 'abc123', 1)); */
-/* console.log(product1.addProduct('producto prueba 2', 'Este es un producto prueba', 200, 'Sin imagen', 'abc124', 25)); */
-/* console.log(product1.addProduct('producto prueba 3', 'Este es un producto prueba', 200, 'Sin imagen', 'abc125', 25));
-console.log(product1.getProductById(1)); */
-/* console.log(product1.getProducts()); */
+productManager
+  .addProduct(product1)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+productManager
+  .addProduct(product2)
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
